fd94d48bbc755f34fa13a3633693c2f2b7b7eb24
jvsibal0312 <jvisbal@nd.edu>
2023-02-09 17:44:28 -0500

    Add Forking

--- Milestone 2/singleshell.c
+++ Milestone 2/singleshell.c
@@ -10,5 +10,21 @@ int main(int argc, char *argv[]){
     fgets(input,512,stdin);     //Get user input
     // printf("You entered: %s", input); //check for the right input 
 
+    int rc = fork(); //create the fork
+    if (rc < 0){
+        // fork failed
+        fprintf(stderr,"Fork Failed\n");
+        exit(1);
+    }
+    else if (rc == 0){
+        //child process
+        // printf("Child Process PID:%d \n", (int) getpid());
+    }
+    else {
+        //parent process
+        // printf("Parent of %d, my pid: %d \n", rc, (int) getpid());
+        
+    }
+
     return 0;
 }
