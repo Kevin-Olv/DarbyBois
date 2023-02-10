d5128b28e1fd3eef576039d54ec6527a09ae3e33
jvsibal0312 <jvisbal@nd.edu>
2023-02-10 18:53:16 -0500

    Adding Comments to Code

--- Milestone 2/singleshell.c
+++ Milestone 2/singleshell.c
@@ -19,6 +19,7 @@ void signalCapture(int nSigNum){
 }
 
 int main(int argc, char *argv[]){
+    
     struct sigaction sigSetValue;
     /* Define the struct */
     sigSetValue.sa_handler = signalCapture;
@@ -28,26 +29,22 @@ int main(int argc, char *argv[]){
     sigaction(SIGTERM, &sigSetValue, NULL);
     sigaction(SIGQUIT, &sigSetValue, NULL);
     sigaction(SIGUSR1, &sigSetValue, NULL);
-    //printf("PID %d cannot be defeated by normal means...\n", getpid());       
+          
+    // Begin Loop
     while(g_bKeepLooking)
     {
     char input[100];
     printf("myshell> ");        //print the prompt
     fgets(input,100,stdin);     //Get user input
     char theCopy[100];
-   /*printf("input: %s\n", input);
-    printf("Length: %lu\n", strlen(input));*/
 
-    strncpy(theCopy,input,strlen(input)-1);    //check the strdup
-    theCopy[strlen(input)-1] = '\0';
-    //printf("You entered: %s", input); //check for the right input 
-    
-    //printf("You entered: %s", myargs[0]); //check for the right input
+    strncpy(theCopy,input,strlen(input)-1);    //Copy the input string into theCopy char array
+    theCopy[strlen(input)-1] = '\0';           // But make sure the last thing is NULL char not \n
 
-
+    // Char array to pass to the child process for exec
     char * myargs[2];
     myargs[0] = strdup(theCopy);
-    myargs[1] = NULL;
+    myargs[1] = NULL; 
 
     int rc = fork(); //create the fork
     if (rc < 0){
@@ -57,18 +54,11 @@ int main(int argc, char *argv[]){
     }
     else if (rc == 0){
         //child process
-        //printf("Child Process PID:%d \n", (int) getpid());
-        //printf("Executing: %s", myargs[0]);
-        
-        /*char * myargs[2];
-        myargs[0] = strdup(theCopy);
-        myargs[1] = NULL;*/
-
-        /*printf("pCopy: %s\n", theCopy);
-        printf("Length: %lu\n", strlen(theCopy));*/
         
         if((execvp(myargs[0], myargs)) < 0){
+            //Error Checking
             printf("exec failed: %s\n",strerror(errno));
+            //Exit Flag for the loop
             g_bKeepLooking = 0;
         }
     }
